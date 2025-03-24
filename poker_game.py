980de2e98a9436029c0d24d1876d4fa313d0193a
Samuel Paladino <118221675+kidkicksam76@users.noreply.github.com>
2025-03-24 08:27:43 -0700

    Update poker_game.py

--- poker_game.py
+++ poker_game.py
@@ -1,1 +1,8 @@
 import random
+
+suits = ['Hearts', 'Diamonds', 'Clubs', 'Spades']
+ranks = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'Jack', 'Queen', 'King', 'Ace']
+
+deck_of_cards = [f"{rank} of {suit}" for suit in suits for rank in ranks]
+
+random.shuffle(deck_of_cards)
